1a11ac03ccdf5d9139f470f81eca0fbeba79b9d3
jucom <julien.combattemlli@hotmail.com>
2018-02-23 10:11:47 +0100

    Add platform.c source file
    Signed-off-by: jucom <julien.combattemlli@hotmail.com>

---
+++ arch/platform/stm32ldisco-rf230/platform.c
@@ -0,0 +1,176 @@
+/*---------------------------------------------------------------------------*/
+#include "contiki.h"
+#include "dev/board.h"
+#include "dev/uart.h"
+#include "dev/leds.h"
+#include "dev/button-sensor.h"
+#include "dev/serial-line.h"
+#include "dev/slip.h"
+#include "lib/random.h"
+#include "net/netstack.h"
+#include "net/mac/framer/frame802154.h"
+#include "net/linkaddr.h"
+#include "sys/platform.h"
+
+#include "stm32l1xx_hal.h"
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+/*---------------------------------------------------------------------------*/
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "STM32LDISCO-RF230"
+#define LOG_LEVEL LOG_LEVEL_MAIN
+/*---------------------------------------------------------------------------*/
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  /* Enable HSE Oscillator and Activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PLLDIV          = RCC_PLL_DIV3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    //Error_Handler();
+  }
+
+  /* Set Voltage scale1 as MCU will run at 32MHz */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Poll VOSF bit of in PWR_CSR. Wait until it is reset to 0 */
+  while (__HAL_PWR_GET_FLAG(PWR_FLAG_VOS) != RESET) {};
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+  clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    //Error_Handler();
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+fade(unsigned char l)
+{
+  volatile int i;
+  int k, j;
+  for(k = 0; k < 800; ++k) {
+    j = k > 400 ? 800 - k : k;
+
+    leds_on(l);
+    for(i = 0; i < j; ++i) {
+      __asm("nop");
+    }
+    leds_off(l);
+    for(i = 0; i < 400 - j; ++i) {
+      __asm("nop");
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+set_rf_params(void)
+{
+  /*uint16_t short_addr; // FIXME
+  uint8_t ext_addr[8];
+
+  ieee_addr_cpy_to(ext_addr, 8);
+
+  short_addr = ext_addr[7];
+  short_addr |= ext_addr[6] << 8;
+
+  NETSTACK_RADIO.set_value(RADIO_PARAM_PAN_ID, IEEE802154_PANID);
+  NETSTACK_RADIO.set_value(RADIO_PARAM_16BIT_ADDR, short_addr);
+  NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, 0); // FIXME: set appropriate value
+  NETSTACK_RADIO.set_object(RADIO_PARAM_64BIT_ADDR, ext_addr, 8); */
+}
+/*---------------------------------------------------------------------------*/
+void
+platform_init_stage_one(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+
+  uart_arch_init();
+  printf("UART is workig !! \r\n");
+
+  leds_init();
+  fade(LEDS_YELLOW);
+}
+/*---------------------------------------------------------------------------*/
+void
+platform_init_stage_two()
+{
+  /*
+   * Character I/O Initialisation.
+   * When the UART receives a character it will call serial_line_input_byte to
+   * notify the core. The same applies for the USB driver.
+   *
+   * If slip-arch is also linked in afterwards (e.g. if we are a border router)
+   * it will overwrite one of the two peripheral input callbacks. Characters
+   * received over the relevant peripheral will be handled by
+   * slip_input_byte instead
+   */
+#if UART_CONF_ENABLE
+  uart_init(0);
+  uart_init(1);
+  uart_set_input(SERIAL_LINE_CONF_UART, serial_line_input_byte);
+#endif
+
+#if USB_SERIAL_CONF_ENABLE
+  usb_serial_init();
+  usb_serial_set_input(serial_line_input_byte);
+#endif
+
+  serial_line_init();
+
+  /* Initialise the H/W RNG engine. */
+  random_init(0);
+
+#if CRYPTO_CONF_INIT
+  crypto_init();
+  crypto_disable();
+#endif
+
+  /* Populate linkaddr_node_addr */
+  //ieee_addr_cpy_to(linkaddr_node_addr.u8, LINKADDR_SIZE); // FIXME
+
+  __enable_irq();
+
+  fade(LEDS_GREEN);
+}
+/*---------------------------------------------------------------------------*/
+void
+platform_init_stage_three()
+{
+  LOG_INFO("%s\n", BOARD_STRING);
+
+  set_rf_params();
+
+  //process_start(&sensors_process, NULL);
+
+  fade(LEDS_GREEN);
+}
+/*---------------------------------------------------------------------------*/
+void
+platform_idle()
+{
+
+}
+/*---------------------------------------------------------------------------*/
+/**
+ * @}
+ * @}
+ */
