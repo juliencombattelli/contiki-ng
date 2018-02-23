a6998cb82bb660e4b399684ce979e504ad4e7630
jucom <julien.combattemlli@hotmail.com>
2018-02-23 09:43:11 +0100

    Add watchdog.c stub file for stm32l1xx cpu
    Signed-off-by: jucom <julien.combattemlli@hotmail.com>

---
+++ arch/cpu/stm32l1xx/watchdog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dev/watchdog.h"
+
+void watchdog_init(void)
+{
+}
+
+void watchdog_start(void)
+{
+}
+
+void watchdog_periodic(void)
+{
+}
+
+void watchdog_stop(void)
+{
+}
+
+void watchdog_reboot(void)
+{
+}
