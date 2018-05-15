5704f9134231e79062f57e739f38027bb71418c1
jucom <julien.combattelli@hotmail.com>
2018-05-15 12:10:15 +0200

    Add some debug log options into contiki-conf.h for development

--- arch/platform/stm32ldisco-rf230/contiki-conf.h
+++ arch/platform/stm32ldisco-rf230/contiki-conf.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <string.h>
+#include "at86rf2xx-contiki-driver.h"
 /*---------------------------------------------------------------------------*/
 /* Include Project Specific conf */
 #ifdef PROJECT_CONF_PATH
@@ -17,7 +18,28 @@
 
 #define USART_CONF_BAUDRATE 115200
 
-#define NETSTACK_CONF_RADIO   at86rf2xx_driver
+#ifndef IEEE_ADDR_CONF_HARDCODED
+#define IEEE_ADDR_CONF_HARDCODED             1
+#endif
+
+#ifndef IEEE_ADDR_CONF_ADDRESS
+#define IEEE_ADDR_CONF_ADDRESS {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef} 
+#endif
+
+#define IEEE802154_CONF_PANID 0x0023
+
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG /* Only for rpl-lite */
+#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_NULLNET                     LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_6TOP                        LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_COAP                        LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_LWM2M                       LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_MAIN                        LOG_LEVEL_DBG                 
+
 
 /*---------------------------------------------------------------------------*/
 /* Include CPU-related configuration */
